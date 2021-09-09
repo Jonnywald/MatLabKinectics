8bdd5d8d6d934a19240442ccf6eb36bd3ba07bdc
Bertola <gpd20001@ad.uconn.edu>
2021-09-09 11:41:20 -0400

    Problem set 2 start

---
+++ PS2.m
@@ -0,0 +1,33 @@
+% Problem set 2
+% Guilherme Bertola
+
+
+% Problem 1)
+% RAW 3.4
+
+% Hex <> MCP + H2  (1)
+% Hex <> 2_MP    (2)
+
+%where: Hex = n-hexane
+%       MCP = Methylcyclopentane
+%       2_MP = 2-methylpentane
+
+%Heats of reaction (Kcal/mol) at 600K
+dHfHex = -46.10;
+dHfMCP = -31.69;
+dHf2_MP = -47.63;
+dHfH2 = 0;
+
+%Gibbs free energy (Kcal/mol) at 600K
+dGfHex = 43.02;
+dGfMCP = 45.76;
+dGf2_MP = 42.39;
+dGfH2 = 0;
+
+% Problem 2)
+% RAW 3.7
+
+% Problem 3)
+% RAW 4.2
+
+% Problem 4)
