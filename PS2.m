43e919bd406a6f28b865e6a07f31d6fc62712a88
Bertola <gpd20001@ad.uconn.edu>
2021-09-13 23:07:35 -0400

    Gave up

--- PS2.m
+++ PS2.m
@@ -148,18 +148,45 @@ T0 = 298; % inital temperature of 298 K
 dH0 = -10; % kcal/mol
 dHvap = 5; % kcal/mol
 K = 8; % K of the reaction
-R = 1.98720425864083; % Gas constant kcal * K-1 * mol-1
-P = 1.0; %atm
-
+R = 1.98720425864083 / 1000; % Gas constant kcal * K-1 * mol-1
+Pt = 1.0; %atm
+V = 100; %L
 %a) over what temperature range does the reactor contain a liquid phase?
 lnPc = c - (dHvap)/(R*T0);
 Pc = exp(lnPc);
 
+% Vapor pressure of C is 0.4012 atm
+
+% T = Pc*V/N*R
+% T = (Pc*V)/((2-z)*R)
+% 0 = (Pc*V)/((2-z)*R) - T
 % van hoff
 
-% Ln (Ka/8) = (-dH0/r)*(1/T - 1/T0)
+% Ln (Ka/8) = (-dH0/R)*(1/T - 1/T0)
+% Ln Ka - Ln 8 = (-dH0/R)*(1/(T)-1/T0)
+% Ka = exp((-dH0/R)*(1/T-1/T0) + ln(8))
 
+% with the activities:
+% Ka = (z/((1-z)*(1-z)))*(2-z)
 
+% 0 =  exp((-dH0/R)*(1/(T)-1/T0) + log(8)) - (z/((1-z)*(1-z)))*(2-z)
+
+x0 = [0.6,500];
+val = fsolve(@(z)prob2(z,dH0,R,T0,Pc,V),x0);
+z = val;
+%calculatin the Temp where Pt = Pc
+T = (Pc*V)/((2-z(1))*R);
+
+% T = 2.026687810582085e+03 K
+
+%b)
+dH0 = 10; %Kcal/mol
+x0 = [0.6,500];
+val = fsolve(@(z)prob2(z,dH0,R,T0,Pc,V),x0);
+z = val;
+T = (Pc*V)/((2-z(1))*R);
+
+% T = 1.604381156135477e+02 K
 
 % Problem 3)
 % RAW 4.2
@@ -172,7 +199,18 @@ Pc = exp(lnPc);
 % [B]      |  0   |    2*zeta      |  (2*zeta)/(1 + zeta)  |
 %----------|------|----------------|-----------------------|
 % Total    |  1   |  1 + zeta(NiT) |           1           |
+ 
+k = 0.35; % min-1
+Pt = 1.0; % atm
+ca0 = 1;
+t = 5; %min
 
+%Lncaca0 = -kt
+%ca = exp(-kt + ln(ca0))
+
+ca = exp(-k*t + log(ca0));
+
+%ca = 0.173773943450445
 
 % Problem 4)
 
@@ -315,7 +353,7 @@ for t = t_range
     %item 4 of van hoff for the reaction 1
     item4_1 = (dd1/(12*R))*((t.^3)-(T0.^3));
     %item 5 of van hoff for the reaction 1
-    item5_1 = (1/R)*(-dHt0_1+da1*t+(db1/2)*(t.^2)+(dc1/3)*(t.^3)+(dd1/4)*(t.^4))*((1/t)-(1/T0));
+    item5_1 = (1/R)*(-dHt0_1+da1*T0+(db1/2)*(T0.^2)+(dc1/3)*(T0.^3)+(dd1/4)*(T0.^4))*((1/t)-(1/T0));
     
     %item 1 of van hoff for the reaction 2
     item1_2 = (da2/R)*log(t/T0);
@@ -326,7 +364,7 @@ for t = t_range
     %item 4 of van hoff for the reaction 2
     item4_2 = (dd2/(12*R))*((t.^3)-(T0.^3));
     %item 5 of van hoff for the reaction 2
-    item5_2 = (1/R)*(-dHt0_2+da2*t+(db2/2)*(t.^2)+(dc2/3)*(t.^3)+(dd2/4)*(t.^4))*((1/t)-(1/T0));
+    item5_2 = (1/R)*(-dHt0_2+da2*T0+(db2/2)*(T0.^2)+(dc2/3)*(T0.^3)+(dd2/4)*(T0.^4))*((1/t)-(1/T0));
     
     %item 1 of van hoff for the reaction 3
     item1_3 = (da3/R)*log(t/T0);
@@ -337,13 +375,19 @@ for t = t_range
     %item 4 of van hoff for the reaction 3
     item4_3 = (dd3/(12*R))*((t.^3)-(T0.^3));
     %item 5 of van hoff for the reaction 3
-    item5_3 = (1/R)*(-dHt0_3+da3*t+(db3/2)*(t.^2)+(dc3/3)*(t.^3)+(dd3/4)*(t.^4))*((1/t)-(1/T0));
+    item5_3 = (1/R)*(-dHt0_3+da3*T0+(db3/2)*(T0.^2)+(dc3/3)*(T0.^3)+(dd3/4)*(T0.^4))*((1/t)-(1/T0));
     
     %calc of the van hoff eqn for each reaction
     lnktkt0_1 = item1_1 + item2_1 + item3_1 + item4_1 + item5_1;
     lnktkt0_2 = item1_2 + item2_2 + item3_2 + item4_2 + item5_2;
     lnktkt0_3 = item1_3 + item2_3 + item3_3 + item4_3 + item5_3;
     
+    %using the aproximation since there may be a bug in the code
+    lnktkt0_1 = (-dHt0_1/R)*(1/t-1/T0);
+    lnktkt0_2 = (-dHt0_2/R)*(1/t-1/T0);
+    lnktkt0_3 = (-dHt0_3/R)*(1/t-1/T0);
+    
+    % calculating the K for each reaction
     Kt1 = exp(lnktkt0_1+log(Kt0_1));
     Kt2 = exp(lnktkt0_2+log(Kt0_2));
     Kt3 = exp(lnktkt0_3+log(Kt0_3));
@@ -377,31 +421,31 @@ for t = t_range
 end
 
 %plot for H2O
-plot(t_range,molH2O);
+plot(t_range,molH2O,"k");
 xlabel("Temperature (K)");
 ylabel("Molar composition");
 title("yH2O over temperature");
 
 %plot for CO2
-plot(t_range,molCO2);
+plot(t_range,molCO2,"k");
 xlabel("Temperature (K)");
 ylabel("Molar composition");
 title("yCO2 over temperature");
 
 %plot for CO
-plot(t_range,molCO);
+plot(t_range,molCO,"k");
 xlabel("Temperature (K)");
 ylabel("Molar composition");
 title("yCO over temperature");
 
 %plot for H2
-plot(t_range,molH2);
+plot(t_range,molH2,"k");
 xlabel("Temperature (K)");
 ylabel("Molar composition");
 title("yH2 over temperature");
 
 %plot for CH4
-plot(t_range,molCH4);
+plot(t_range,molCH4,"k");
 xlabel("Temperature (K)");
 ylabel("Molar composition");
 title("yCH4 over temperature");
@@ -412,7 +456,10 @@ function F = prob4(z,Kt1,Kt2,Kt3)
     F(2) = ((z(2)*(2*z(1)+z(2)-2*z(3)))/(1-2*z(1)-z(2)))*(1/(1+z(1)+z(2)+z(3)))- Kt2;
     F(3) = (z(3)/((2*z(1)+z(2)-2*z(3)).^2))*(1+z(1)+z(2)+z(3))- Kt3;
 end
-
+function w = prob2(z,dH0,R,T0,Pc,V)
+    w(1) = exp((-dH0/R)*(1/(z(2))-1/T0) + log(8)) - (z(1)/((1-z(1))*(1-z(1))))*(2-z(1));
+    w(2) = (Pc*V)/((2-z(1))*R) - z(2);
+end
 function u = eqn(z)
     u(1) = ((z(1).^2)/(1-z(1)-2*(z(1).^2)))-exp(((-14.410000000000000)/(0.082057366080960))*((1/z(2))-(1/600))+log(0.945868124008406));
     u(2) = (z(1)/(1-2*z(1)))-exp(((1.530000000000001)/(0.082057366080960))*((1/z(2))-(1/600))+log(1.012878144368649));
