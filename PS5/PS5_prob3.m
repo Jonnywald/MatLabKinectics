fc1fa75b8b36e0ddb68fc1e7d6a834e13e807b5d
Bertola <gpd20001@ad.uconn.edu>
2021-09-30 16:37:13 -0400

    Started PS5

---
+++ PS5/PS5_prob3.m
@@ -0,0 +1,14 @@
+%problem set 5
+% problem 3
+
+% RAW 6.14
+
+% A <-> B
+% PFR adiabatic
+
+% Species | MolFeed Fi0| Exit (Fi) |  Exit    |     
+%---------|------------|-----------|----------|
+% A       |    Fa0     |    Fa     |  Fa0 - z |
+% B       |    Fb0     |    Fb     |  Fb0 + z |
+%---------|------------|-----------|----------|
+% Total   |Ff = Fa0+Fb0|    FT     |     1    |
