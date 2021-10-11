b5f28ce091c1a7adef4d04fd4bbbfdce88b4b4f2
Bertola <gpd20001@ad.uconn.edu>
2021-10-11 17:26:27 -0400

    Progress on prob 1 and 2 of PS6

--- PS6/PS6_prob1.m
+++ PS6/PS6_prob1.m
@@ -44,16 +44,86 @@ dHr = dHr * 1000; %converting kcal/MolA to cal/MolA
 %Enrg. Balance
 % dTdt = (-ra*Vr*-dHr)/(Ca*Vr*Cpa)
 
+
+%A)
 y0 = [Ca0, Tr];
 t_span = linspace(0,10,500);
 [t,x] = ode45(@(t,x)part_a(t,x,km,EaR,Tm,VR,Cpa,dHr),t_span,y0);
 
 Ca = x(:,1);
 Temp = x(:,2);
-subplot(1,2,1);
-plot(t,Ca);
-subplot(1,2,2);
+
+Temp_rise = Temp(length(Temp))-Temp(1); %3266.5 K
+
+%B)
+t_span = linspace(0,1000,1001);
+[t,x] = ode45(@(t,x)part_b(t,x,km),t_span,Ca0);
+Ca = x(:,1);
+
+Xa = (Ca0 - Ca)./Ca0;
+
+for i=1:1001
+    if Xa(i)>=0.95
+        Time = t(i); %552 min
+        break
+    end
+end
+
+%time to achive 0.95
+% 552 Min
+
+%C)
+y0 = [Ca0, Tr];
+t_span = linspace(0,10,500);
+[t,x] = ode45(@(t,x)part_c(t,x,km,EaR,Tm,VR,Cpa,dHr),t_span,y0);
+
+Ca = x(:,1);
+Temp = x(:,2);
+
+Xa = (Ca0 - Ca)./Ca0;
+for i=1:500
+    if Xa(i)>=0.95
+        Time = t(i); %3.6 min
+        break
+    end
+end
+plot(Temp,Ca);
+xlabel("Temperature (K)");
+ylabel("Concentration (mol/L)");
+title("Temperature x Concentration of A");
+
+%D)
+Ua = 0.01 * 1000; % Cal/(min L K)
+Ta = 300; %K
+y0 = [Ca0, Tr];
+t_span = linspace(0,3,500);
+[t,x] = ode45(@(t,x)part_d(t,x,km,EaR,Tm,VR,Cpa,dHr,Ta,Ua),t_span,y0);
+
+Ca = x(:,1);
+Temp = x(:,2);
+
+plot(Temp,Ca);
+xlabel("Temperature (K)");
+ylabel("Concentration (mol/L)");
+title("Temperature x Concentration of A");
+
+%E)
+Ua = 0.01 * 1000; % Cal/(min L K)
+Ta = 300; %K
+y0 = [Ca0, Tr];
+t_span = linspace(0,3,500);
+[t,x] = ode45(@(t,x)part_e(t,x,km,EaR,Tm,VR,Cpa,dHr,Ta,Ua),t_span,y0);
+
+Ca = x(:,1);
+Temp = x(:,2);
+
+% plot(Temp,Ca);
+% xlabel("Temperature (K)");
+% ylabel("Concentration (mol/L)");
+% title("Temperature x Concentration of A");
+
 plot(t,Temp);
+
 function f=part_a(t,x,km,EaR,Tm,VR,Cpa,dHr)
     Ca = x(1);
     T = x(2);
@@ -62,6 +132,48 @@ function f=part_a(t,x,km,EaR,Tm,VR,Cpa,dHr)
     ra = -k * Ca^2;
     dCadt = ra;
     dTdt = (-ra*VR*-dHr)/(Ca*VR*Cpa);
+    f = [dCadt;dTdt];
+end
+
+function f=part_b(t,x,k)
+    Ca = x(1);
+    ra = -k * Ca^2;
+    dCadt = ra;
+
+    f = [dCadt];
+end
+
+function f=part_c(t,x,km,EaR,Tm,VR,Cpa,dHr)
+    Ca = x(1);
+    T = x(2);
+    
+    k = km*exp(-EaR*(1/T - 1/Tm));
+    ra = -k * Ca^2;
+    dCadt = ra;
+    dTdt = ((-ra)*VR*(-dHr))/(Ca*VR*Cpa);
+    f = [dCadt;dTdt];
+end
+
+function f=part_d(t,x,km,EaR,Tm,VR,Cpa,dHr,Ta,Ua)
+    Ca = x(1);
+    T = x(2);
+    
+    k = km*exp(-EaR*(1/T - 1/Tm));
+    ra = -k * Ca^2;
+    Q = Ua*(T - Ta)*VR;
+    dCadt = ra;
+    dTdt = (Q + (-ra*VR*-dHr))/(Ca*VR*Cpa);
+    f = [dCadt;dTdt];
+end
+function f=part_e(t,x,km,EaR,Tm,VR,Cpa,dHr,Ta,Ua)
+    Ca = x(1);
+    T = x(2);
+    
+    k = km*exp(-EaR*(1/T - 1/Tm));
+    ra = -k * Ca^2;
+    Q = Ua*(T - Ta)*VR;
+    dCadt = ra;
+    dTdt = (Q + (-ra*VR*-dHr))/(Ca*VR*Cpa);
     f = [dCadt;dTdt];
 end
 
--- PS6/PS6_prob2.m
+++ PS6/PS6_prob2.m
@@ -3,7 +3,38 @@
 
 %RAW 6.5
 
-% A -> B
-% A -> C
+% A -> B (1)
+% A -> C (2)
 
 Ca0 = 5.0; %mol / L
+Vr = 1000; % L
+V0 = 100; % L/min
+Cp = 0.22; %cal/(g K)
+dHr1 = -12e3; %Cal/Mol
+dHr2 = -15e3; %cal/Mol
+Mf = 93.2e3; %g/min
+
+T0 = 45 + 273.15; %K
+
+Selectivity = 5;
+
+function f=prob2(x)
+
+Ca = x(1);
+Cb = x(2);
+Cc = x(3);
+T = x(4);
+
+
+k1 = 3.16e14 * exp(-12500/T);
+k2 = 2.52e9 * exp(-8500/T);
+
+r1 = k1 * Ca;
+r2 = k2 * Ca;
+
+ra = -r1 -r2;
+rb = r1;
+rc = r2;
+
+
+end
