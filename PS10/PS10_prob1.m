85ffd2bda9135d80a34c650fde5bf12332e49ce2
Bertola <gpd20001@ad.uconn.edu>
2021-11-22 17:34:07 -0500

    PS10 prob 1 finished?

--- PS10/PS10_prob1.m
+++ PS10/PS10_prob1.m
@@ -8,18 +8,36 @@ rH = 120; %pm
 
 rGa = rGa * 0.01;% A
 rH = rH * 0.01;% A
-dGaH = 2.47032; % A
+dGaH = 2.60108; % A
 
-dGaHCH3 = 2.58191; %A
+dGaHCH3 = 2.66043; %A
 
 DGaNH = dGaH + rGa + rH; %A
 
 DGaCH3 = dGaHCH3 + rGa + rH; %A
 
 dAB = (DGaCH3 + DGaNH)/2; %A
+dAB = dAB * 1e-10;  %5.7008e-10 m
 
+%molar masses
 M_GaNH = 84.74;
 M_GaCH3 = 84.76;
 
-M = (M_GaCH3*M_GaNH)/(M_GaNH+M_GaCH3);
+M = (M_GaCH3*M_GaNH)/(M_GaNH+M_GaCH3); %42.374999410029496
+
+
+%boltzmann constant
+kb = 1.38064852e-23; %m2 kg s-2 K-1
+
+%velocity
+Ur = sqrt((8*kb*T)/(M*pi));
+
+%Steric Factor NonLinear Molecule + NonLinear Molecule -> NonLinear Complex
+P = 1e-5;
+
+%collision theory constant
+Kcoll = pi*dAB^2*Ur*exp(-E/(kb*T));
+
+% final rate constant
+K = P * Kcoll;
 
