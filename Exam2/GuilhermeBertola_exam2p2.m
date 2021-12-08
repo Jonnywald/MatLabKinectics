7fb8f2df9567496af5f798761625810f7dda955a
Bertola <gpd20001@ad.uconn.edu>
2021-12-08 18:14:54 -0500

    exam 2 finished

--- Exam2/GuilhermeBertola_exam2p2.m
+++ Exam2/GuilhermeBertola_exam2p2.m
@@ -8,38 +8,38 @@
 %experimental data: time (min) per pressure (torr)
 data = [0 268.7; 20 293.0; 40 302.2; 60 311.0; 80 318.9;...
     100 325.9; 120 332.3; 140 338.8; 160 344.4];
-%equilibrium pressure
-Eq_pressure = 473.0;
 
 %separating the values
 t = data(:,1);
 P = data(:,2);
 
-%temperature
-T = 298; %K
-
-%initial pressure of N2O5
-Pa0 = 268.7; %torr
-
-
-
 %equilibrium constant
-K = 97.5; %torr
+Kp = 97.5; %torr
 
-k = nlinfit(t,P,@func,K);
-
+para = 0.0058; %guess
+k = nlinfit(t,P,@func,para);
+k = real(k); %8.572e-04 min-1
 newP = func(k,t);
+
 
 hold on;
 plot(t,P,"k*");
 plot(t,newP,"k-");
 xlabel("time (min)");
 ylabel("pressure (torr)");
+title("Pressure v Time");
+legend("Experimental","Predicted");
 hold off;
 
-%-ra = KPa
-
-% Pa = 
 function p=func(k,t)
-    p = 1.5*268.7 - 0.5*268.7*exp(-k*t);
+     %initial pressure of N2O5
+     Pa0 = 268.7; %torr
+     %equilibrium constant
+     Kp = 97.5; %torr
+     Pa = Pa0 * exp(-k*t);
+     Pb = Pa0 - Pa;
+     Pc = (Pa0 - Pa)/2;
+     Pd = sqrt(Pb*Kp);
+
+     p = Pa + Pb + Pc + Pd;
 end
